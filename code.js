c66f8ada34c74fdf3407a550ef75a240ecdb20bd
jakebrady5 <jakebrady5@gmail.com>
2016-01-30 19:16:00 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(flav){
+  return "I love " + flav;
+}
+
+function shouting(str){
+  return str.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num)
+}
+
+function theTruth(){
+  return !!1;
+}
